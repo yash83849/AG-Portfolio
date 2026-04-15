dbcd6caa73856779ce3a6d4cf13ebdbd72417f2f
yash83849 <yash83849@gmail.com>
2026-04-15 17:51:41 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
-  base: '/portfolio/',
+  base: '/AG-Portfolio/',
 })
